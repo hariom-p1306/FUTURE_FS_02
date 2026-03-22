e648bfcba202c72bda1364700ce307ef72716610
Delta Student <hp3432884@gmail.com>
2026-03-22 14:38:27 +0530

    api changes

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,12 @@ connectDB();
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/leads", require("./routes/leadRoutes"));
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running...");
+// app.listen(process.env.PORT, () => {
+//   console.log("Server running...");
+// });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://future-fs-02-9y5v.onrender.com/api"
 });
 
-// Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
